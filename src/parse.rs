834fab376d2ba44016509ccb331347e8585559a6
James Angus <james@ejangi.com>
2024-06-02 19:19:20 +1000

    Get the image_url for each book.

--- src/parse.rs
+++ src/parse.rs
@@ -6,22 +6,27 @@ pub mod parse {
     pub fn get_books_from_html(body: String) -> Vec<BookBox> {
         // document.querySelectorAll('.bigBoxContent .bookBox');
         let book_box_selector = Selector::parse(".bigBoxContent .bookBox").unwrap();
-        let image_alt = Selector::parse(r#"a img[alt]"#).unwrap();
+        let image_element = Selector::parse(r#"a img[alt]"#).unwrap();
         let document = Html::parse_document(&body);
         let book_boxes_elements = document.select(&book_box_selector);
         let mut book_boxes:Vec<BookBox> = Vec::new();
 
         for book_box_element in book_boxes_elements {
             let mut title = "";
+            let mut image_url = "";
 
-            if let Some(alt) = book_box_element.select(&image_alt).next().unwrap().value().attr("alt") {
+            if let Some(alt) = book_box_element.select(&image_element).next().unwrap().value().attr("alt") {
                 title = alt;
             }
 
-            let book_box = BookBox::new(title.to_string(), "".to_string(), "".to_string(), None, None);
+            if let Some(src) = book_box_element.select(&image_element).next().unwrap().value().attr("src") {
+                image_url = src;
+            }
+
+            let book_box = BookBox::new(title.to_string(), image_url.to_string(), "".to_string(), None, None);
             book_boxes.push(book_box);
 
-            println!("Title: {}", title);
+            println!("Image: {}", image_url);
         }
 
         return book_boxes;
