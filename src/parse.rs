9ee5348cc3500468806ac6227055804521a461cd
James Angus <james@ejangi.com>
2024-06-03 17:36:02 +1000

    Parse the HTML and convert it into a Vec<BookBox> that we can use elsewhere

--- src/parse.rs
+++ src/parse.rs
@@ -1,6 +1,9 @@
 pub mod parse {
     use scraper::{Html, Selector};
     use crate::types::BookBox;
+    use regex::Regex;
+    use htmlentity::entity::{decode, ICodedDataTrait};
+    use htmlentity::types::Byte;
 
     const GOODREADS_BASENAME: &str = "https://goodreads.com";
 
@@ -9,6 +12,9 @@ pub mod parse {
         let book_box_selector = Selector::parse(".bigBoxContent .bookBox").unwrap();
         let image_element = Selector::parse(r#"a img[alt]"#).unwrap();
         let a_element = Selector::parse("a[href]").unwrap();
+        let rating_avg_regex = Regex::new(r#"([0-5]{1}\.[0-9]+)\savg\srating"#).unwrap();
+        let rating_count_regex = Regex::new(r#"([0-9,]{3,})\sratings"#).unwrap();
+
         let document = Html::parse_document(&body);
         let book_boxes_elements = document.select(&book_box_selector);
         let mut book_boxes:Vec<BookBox> = Vec::new();
@@ -17,6 +23,8 @@ pub mod parse {
             let mut title = String::from("");
             let mut url = String::from("");
             let mut image_url = String::from("");
+            let mut rating_avg: Option<f32> = None;
+            let mut rating_count: Option<i32> = None;
 
             if let Some(alt) = book_box_element.select(&image_element).next().unwrap().value().attr("alt") {
                 title = alt.to_string();
@@ -24,25 +32,45 @@ pub mod parse {
 
             if let Some(href) = book_box_element.select(&a_element).next().unwrap().value().attr("href") {
                 url = href.to_string();
+                let url_first_6: String = url.chars().take(6).collect();
+    
+                if url.len() > 5 && url_first_6 != "https" {
+                    url = GOODREADS_BASENAME.to_string() + &url;
+                }
             }
 
-            let url_first_6: String = url.chars().take(6).collect();
-
-            if url.len() > 5 && url_first_6 != "https" {
-                url = GOODREADS_BASENAME.to_string() + &url;
-            }
 
             if let Some(src) = book_box_element.select(&image_element).next().unwrap().value().attr("src") {
                 image_url = src.to_string();
             }
 
-            let book_box = BookBox::new(title.to_string(), image_url.to_string(), "".to_string(), None, None);
-            book_boxes.push(book_box);
+            if let Some(rating_avg_matches) = rating_avg_regex.captures(&book_box_element.html()) {
+                if let Some(rating_avg_match) = rating_avg_matches.get(1) {
+                    rating_avg = Some(rating_avg_match.as_str().parse::<f32>().unwrap());
+                }
+            }
 
-            println!("URL: {}", url);
+            if let Some(rating_count_matches) = rating_count_regex.captures(&book_box_element.html()) {
+                if let Some(rating_count_match) = rating_count_matches.get(1) {
+                    rating_count = Some(rating_count_match.as_str().replace(",", "").parse::<i32>().unwrap());
+                }
+            }
+
+
+            let book_box = BookBox::new(title, url, image_url, rating_avg, rating_count);
+            book_boxes.push(book_box);
         }
 
         return book_boxes;
+    }
+
+    pub fn html_decode(encoded_data: String) -> String {
+        let bytes = encoded_data
+            .as_bytes()
+            .into_iter()
+            .map(|byte| *byte)
+            .collect::<Vec<Byte>>();
+        return decode(&bytes).to_string().unwrap();
     }
 
     #[tokio::test]
@@ -56,6 +84,10 @@ pub mod parse {
 
         for book in books {
             assert!(book.title.len() > 0);
+            assert!(book.url.len() > 0);
+            assert!(book.image_url.len() > 0);
+            assert!(book.rating_avg.unwrap_or(0.0) > 0.0);
+            assert!(book.rating_count.unwrap_or(0) > 0);
         }
     }
 }
